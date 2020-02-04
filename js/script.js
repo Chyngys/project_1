9c9f143a8fd49f1a1fb000373f01f0ec8c77809c
Chyngys <chynga2002@mail.ru>
2020-02-04 20:48:10 +0600

    elements from site are taken

--- js/script.js
+++ js/script.js
@@ -83,7 +83,32 @@ for(let i in appData){
     console.log("Наша программа включает в себя данные: " + i + " - " + appData[i]);
 }
 
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value'),
+    dayBudgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value');
 
+let expensesItem = document.getElementsByClassName('expenses-item');
+
+let expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalexpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2];
+
+let optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item');
+
+let incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+ 
 
 
 
